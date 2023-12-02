c4b4b889eb424dd572d1d3a7704201e4e6973317
jktarnoff <justin.tarnoff@gmail.com>
2023-12-01 22:20:02 -0600

    Completed Day 1

--- src/2023/Day01/Day01.kt
+++ src/2023/Day01/Day01.kt
@@ -1,17 +1,59 @@
+package Day01
+
+import println
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            it.toDigitString().toCalibrationValue()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val matcher = Regex("""(?=(one|two|three|four|five|six|seven|eight|nine))""")
+        return input.map { str ->
+            var newStr = str
+            matcher.findAll(str).forEach {
+                newStr = newStr.replaceRange(it.range.first, it.range.first + 1, changeToDigit(it))
+            }
+            newStr
+        }.sumOf {
+            it.toDigitString().toCalibrationValue()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    val testInput2 = readInput("Day01_test2")
     val input = readInput("Day01")
+
+//    part1(testInput).println()
     part1(input).println()
+
+    part2(testInput2).println()
     part2(input).println()
+}
+fun String.toDigitString(): String {
+    return this.filter { chr -> chr.isDigit() }
+}
+fun String.toCalibrationValue(): Int {
+    return (this.first().toString() + this.last()).toInt()
+}
+
+fun changeToDigit(result: MatchResult): CharSequence {
+    val digitMap = mapOf<CharSequence, Int>(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+    println(result.groupValues[1])
+
+    return digitMap[result.groupValues[1]].toString()
 }
